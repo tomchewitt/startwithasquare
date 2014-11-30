4e90301dce4251cb561a79730b01943abad4eca6
Tom Hewitt <tom@colourflood.co.uk>
2014-11-30 18:09:48 +0000

    initial build w/ minimal styling

--- single.php
+++ single.php
@@ -2,26 +2,30 @@
 
 	 <!-- Start the Loop. -->
 	 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		
-		<article class="post post-list">
-			<a href="<?php the_permalink(); ?>">
-				<div class="post-inner">
-				 	<h1><?php the_title(); ?></h1>
 
-				 	<div class="meta">
+	 	<section class="breadcrumbs">
+	 		<p><span><a href="/">Home</a></span> // <span><?php the_title(); ?></span></p>
+	 	</section>		
+
+		<article class="post single">
+			<div class="post-inner">
+			 	<h1><?php the_title(); ?></h1>
+
+			 	<div class="meta">
 				 		<span class="genericon genericon-time"></span> <span class="meta-info"><?php the_date(); echo ' '; the_time(); ?></span>
 				 		<span class="genericon genericon-user"></span> <span class="meta-info"><?php the_author(); ?></span>
 				 		<span class="genericon genericon-tag"></span> <span class="meta-info"><?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' ';} ?></span>
  					</div>
 
-				 	<section class="post-content">
-				 		<?php the_content( '' , TRUE ); ?>
-				 	</section>
-
-				 	<span class="read-more"><span class="genericon genericon-standard"></span> <span class="meta-info">READ MORE</span></span>
-				</div>
-			</a>
+			 	<section class="post-content">
+			 		<?php the_content(); ?>
+			 	</section>
+			</div>
 		</article>
+
+		<!-- 
+			SHARE?
+		 -->
 
 	 <?php endwhile; else : ?>
 	 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
