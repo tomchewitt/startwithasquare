d17061c0950c8cc8f5b381cdb7839b8be63d27d4
Tom Hewitt <tom@colourflood.co.uk>
2014-11-30 03:27:22 +0000

    added header logo and styling

--- header.php
+++ header.php
@@ -6,13 +6,36 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
-	<link type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
+	<link href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" rel="stylesheet">
 	<!-- <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script> -->
 </head>
 
 <body <?php body_class(); ?>>
 
 	<header>
-		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<div class="beta">
+			<p>While the full version of this site is having it's bells and whistles finalised, please enjoy this 'lite' version for now :)</p>
+		</div>
+
+		<h1 class="logo">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 850 850">
+				<style type="text/css">
+					.st0{fill:#E64C3B;}
+					.st1{fill:#AA2624;}
+					.st2{fill:#EE7E7B;}
+				</style>
+				<rect class="st0" width="850" height="850"/>
+				<polygon class="st1" points="384.6,237.9 384.6,424.1 585.7,540.7 585.7,354.6 "/>
+				<polygon class="st0" points="384.5,286.2 384.3,472.3 424.8,449 425,262.9 "/>
+				<polygon class="st1" points="223.9,331.6 223.9,517.8 424.9,634.4 424.9,448.3 "/>
+				<polygon class="st1" points="304.1,285.5 304.1,471.7 505.2,588.3 505.2,402.2 "/>
+				<polygon class="st0" points="424.9,448.3 424.7,634.4 545.7,564.6 545.8,378.5 "/>
+				<polygon class="st0" points="585.8,355.2 585.6,541.4 626.1,518 626.3,331.9 "/>
+				<polygon class="st2" points="424.8,215.6 626.3,332 585.7,355 424.8,262.4 384.6,285.5 545.8,378.5 424.9,448.3 223.7,331.9 264.2,309.6 424.2,401.3 464.7,379.2 304.1,285.5 "/>
+			</svg>
+
+			<span class="word">start with a square</span>
+
+		</h1>
 	</header>
 	<section class="wrap">
